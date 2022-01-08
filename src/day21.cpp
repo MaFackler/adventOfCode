ac24a89f19342dc540afbde59892d5b5b27756e8
MaFackler <17232532+MaFackler@users.noreply.github.com>
2022-01-08 16:44:13 +0100

    Day 21 - Part Two

--- src/day21.cpp
+++ src/day21.cpp
@@ -4,17 +4,51 @@
 #include <vector>
 #include <algorithm>
 #include <assert.h> 
+#include <map>
 
 using std::string;
 using std::cout;
 using std::vector;
+using std::tuple;
+using std::map;
+
+static map<tuple<size_t, size_t, size_t, size_t>, tuple<size_t, size_t>> cache;
 
 struct Player {
-    int pos = 0;
-    int score = 0;
+    size_t pos = 0;
+    size_t score = 0;
 
-    Player(int pos) : pos(pos) {};
+    Player(size_t pos) : pos(pos) {};
 };
+
+
+tuple<size_t, size_t> count_universe(size_t p1, size_t p2, size_t s1, size_t s2) {
+    if (s1 >= 21) {
+        return tuple<size_t, size_t>(1, 0);
+    }
+    if (s2 >= 21) {
+        return tuple<size_t, size_t>(0, 1);
+    }
+    auto to_search = std::make_tuple(p1, p2, s1, s2);
+    if (cache.find(to_search) != cache.end())  {
+        return cache[to_search];
+    }
+    auto wins = tuple<size_t, size_t>(0, 0);
+    for (size_t d1 = 1; d1 <= 3; ++d1) {
+        for (size_t d2 = 1; d2 <= 3; ++d2) {
+            for (size_t d3 = 1; d3 <= 3; ++d3) {
+                size_t pnew = p1 + d1 + d2 + d3;
+                pnew = pnew % 10;
+                size_t snew = s1 + pnew + 1;
+                auto wins_of_player = count_universe(p2, pnew, s2, snew);
+                wins = tuple<size_t, size_t>(std::get<1>(wins_of_player) + std::get<0>(wins),
+                                       std::get<0>(wins_of_player) + std::get<1>(wins));
+            }
+        }
+    }
+    cache[to_search] = wins;
+    return wins;
+}
 
 int main() {
     std::ifstream fp("data/input_day21.txt");
@@ -31,6 +65,8 @@ int main() {
             assert(players.size() == player);
         }
     }
+
+    auto res2 = count_universe(players[0].pos, players[1].pos, 0, 0);
 
     int player_index = 0;
     int dice_counter = 0;
@@ -63,13 +99,15 @@ int main() {
     auto looser = std::min_element(players.cbegin(),
                                    players.cend(),
                                    [](const Player &a, const Player &b) { return a.score < b.score; });
-    int min_score = (*looser).score;
-    int res = roll_count * min_score;
+    size_t min_score = (*looser).score;
+    size_t res = roll_count * min_score;
 
     //for (Player &p: players) {
     //    std::cout << "Player score is " << p.score << std::endl;
     //}
 
     std::cout << res << "\n";
+    auto res22 = std::max(std::get<0>(res2), std::get<1>(res2));
+    std::cout << res22 << "\n";
 
 }
--- tasks/day21.md
+++ tasks/day21.md
@@ -32,3 +32,14 @@ Since player 1 has at least 1000 points, player 1 wins and the game ends. At thi
 Play a practice game using the deterministic 100-sided die. The moment either player wins, what do you get if you multiply the score of the losing player by the number of times the die was rolled during the game?
 
 Your puzzle answer was *893700*.
+
+# Part Two
+
+Now that you're warmed up, it's time to play the real game.
+A second compartment opens, this time labeled Dirac dice. Out of it falls a single three-sided die.
+As you experiment with the die, you feel a little strange. An informational brochure in the compartment explains that this is a quantum die: when you roll it, the universe splits into multiple copies, one copy for each possible outcome of the die. In this case, rolling the die always splits the universe into three copies: one where the outcome of the roll was 1, one where it was 2, and one where it was 3.
+The game is played the same as before, although to prevent things from getting too far out of hand, the game now ends when either player's score reaches at least 21.
+Using the same starting positions as in the example above, player 1 wins in 444356092776315 universes, while player 2 merely wins in 341960390180808 universes.
+Using your given starting positions, determine every possible outcome. Find the player that wins in more universes; in how many universes does that player win?
+
+Your puzzle answer was *568867175661958*.
