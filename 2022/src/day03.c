a6a4bf300a3c77d6bb8bf3e5c0f5fb1082bd1061
MaFackler <17232532+MaFackler@users.noreply.github.com>
2022-12-03 15:22:25 +0100

    2022 Day03 - Part2

--- 2022/src/day03.c
+++ 2022/src/day03.c
@@ -2,17 +2,54 @@
 #include <string.h>
 #include <assert.h>
 
+int counters[3][256] = {0};
+
+int get_value(char c) {
+    int value = 0;
+    if (c >= 'a' && c <= 'z') {
+        value = c - 'a' + 1;
+    }
+    if (c >= 'A' && c <= 'Z') {
+        value = c - 'A' + 27;
+    }
+    assert(value != 0);
+    return value;
+}
+
 int main() {
     char *line = NULL;
     ssize_t n = 0;
     size_t a = 0;
     int res = 0;
+    int res2 = 0;
 
+    int line_count = 0;
     while ((n = getline(&line, &a, stdin)) != -1) {
         size_t len = strlen(line) - 1;
         size_t halflen = len / 2;
         assert(len % 2 == 0);
         char c = 0;
+
+        for (int i = 0; i < len; ++i) {
+            counters[line_count][(int) line[i]]++;
+        }
+
+        if ((++line_count % 3) == 0) {
+
+            for (int i = 0; i < 256; ++i) {
+                if (counters[0][i] > 0 && counters[1][i] > 0 && counters[2][i] > 0) {
+                    //int res2 = counters[0][i] + counters[1][i] + counters[2][i];
+                    //printf("got %c %d %d", i, get_value(i), res2);
+                    res2 += get_value(i);
+                }
+            }
+            for (int i = 0; i < 3; ++i) {
+                for (int j = 0; j < 256; ++j) {
+                    counters[i][j] = 0;
+                }
+            }
+            line_count = 0;
+        }
 
         for (int i = 0; i < halflen; ++i) {
             for (int j = 0; j < halflen; ++j) {
@@ -24,16 +61,8 @@ int main() {
             }
         }
         skip:
-        int value = 0;
-        if (c >= 'a' && c <= 'z') {
-            value = c - 'a' + 1;
-        }
-        if (c >= 'A' && c <= 'Z') {
-            value = c - 'A' + 27;
-        }
-        assert(value != 0);
-        res += value;
+        res += get_value(c);
     }
-    printf("%d\n", res);
+    printf("%d\n", res2);
 
 }
