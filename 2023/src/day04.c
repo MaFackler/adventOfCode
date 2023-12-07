7262a47b002b5a651a5636ba827d36c01c31854a
MaFackler <17232532+MaFackler@users.noreply.github.com>
2023-12-07 17:22:03 +0100

    2023 - Day 4 - Part 2

--- 2023/src/day04.c
+++ 2023/src/day04.c
@@ -4,6 +4,7 @@ int main() {
     char *line = NULL;
     int res = 0;
     list(int) numbers[2] = {0};
+    int card_map[512] = {0};
     while (readline(&line)) {
         char *cards = buffer(1024);
         int id = 0;
@@ -18,9 +19,13 @@ int main() {
         }
 
         int value = 0;
+        int index = id - 1;
+        int copies = ++card_map[index];
+        int amount_matching = 0;
         for each(roll, numbers[1]) {
             for each(win, numbers[0]) {
                 if (*roll == *win) {
+                    amount_matching++;
                     if (value == 0) {
                         value = 1;
                     } else {
@@ -29,9 +34,16 @@ int main() {
                 }
             }
         }
+        for (int j = 0; j < amount_matching; ++j) {
+            card_map[index + 1 + j] += copies;
+        }
         vec_clear(numbers[0]);
         vec_clear(numbers[1]);
-        res += value;
+        // res += value;
+    }
+
+    for (int i = 0; i < ArraySize(card_map); ++i) {
+        res += card_map[i];
     }
 
     print("%d", res);
