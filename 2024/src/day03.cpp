2cee3ccb08754d32e4a260e1637f76f052501dcb
MaFackler <17232532+MaFackler@users.noreply.github.com>
2024-12-03 16:58:39 +0100

    2024 - Day03

---
+++ 2024/src/day03.cpp
@@ -0,0 +1,114 @@
+#include "advent.hpp"
+
+enum struct TokenKind {
+    MUL,
+    DO,
+    DONT,
+    END,
+};
+
+struct Token {
+    TokenKind kind;
+    size_t start;
+    size_t end;
+    struct {
+        int first;
+        int second;
+    } mul;
+};
+
+
+#define EXPECT(v) c = stream.peek(); \
+    if (c != v) { goto read; } \
+    c = stream.get()
+
+
+
+Token GetToken(istringstream &stream) {
+    Token res = {};
+
+read:
+    if (!stream.good()) {
+        res.kind = TokenKind::END;
+        return res;
+    }
+    char c = stream.get();
+
+    if (c == 'm') {
+        res.start = (int) stream.tellg() - 1;
+        EXPECT('u');
+        EXPECT('l');
+        EXPECT('(');
+        assert(std::isdigit(stream.peek()));
+        int first = 0;
+        stream >> first;
+        EXPECT(',');
+        assert(std::isdigit(stream.peek()));
+        int second = 0;
+        stream >> second;
+        EXPECT(')');
+        res.kind = TokenKind::MUL;
+        res.mul.first = first;
+        res.mul.second = second;
+        res.end = stream.tellg();
+        return res;
+    } else if (c == 'd') {
+        res.start = (int) stream.tellg() - 1;
+        EXPECT('o');
+        if (stream.peek() == '(') {
+            // do
+            stream.get();
+            EXPECT(')');
+            res.kind = TokenKind::DO;
+            res.end = stream.tellg();
+            return res;
+        } else {
+            // don't
+            EXPECT('n');
+            EXPECT('\'');
+            EXPECT('t');
+            EXPECT('(');
+            EXPECT(')');
+            res.kind = TokenKind::DONT;
+            res.end = stream.tellg();
+            return res;
+        }
+    }
+
+    goto read;
+
+
+    return res;
+}
+
+int main() {
+    auto fp = ifstream("data/day03.txt");
+
+    int res1 = 0;
+    int res2 = 0;
+    bool enabled = true;
+    for (string line; getline(fp, line);) {
+        istringstream stream(line);
+        Token token = {};
+        while ((token = GetToken(stream)).kind != TokenKind::END) {
+            string value = line.substr(token.start, token.end - token.start);
+            switch (token.kind) {
+                case TokenKind::MUL: {
+                    //print() << value;
+                    int value = token.mul.first * token.mul.second;
+                    res1 += value;
+                    res2 += enabled ? value : 0;
+                } break;
+                case TokenKind::DO:
+                    enabled = true;
+                    break;
+                case TokenKind::DONT: {
+                    enabled = false;
+                } break;
+            }
+        }
+    }
+    print() << "Solution1: " << res1;
+    print() << "Solution2: " << res2;
+    return 0;
+}
--- 2024/src/demo.cpp
+++ 2024/src/demo.cpp
@@ -1,64 +1,15 @@
 #include "advent.hpp"
 
 
-bool checkLevels(vector<int> levels) {
-    auto differences = std::views::iota(1, int(levels.size()))
-        | std::views::transform([&levels](int i) {
-            return levels[i - 1] - levels[i];
-        });
-
-    auto increasing = std::ranges::all_of(differences, [](auto ele) { return ele >= 0; });
-    auto decreasing = std::ranges::all_of(differences, [](auto ele) { return ele <= 0; });
-    auto inrange = std::ranges::all_of(differences, [](auto ele) { return 1 <= abs(ele) && abs(ele) <= 3; });
-    bool res = (increasing || decreasing) && inrange;
-    return res;
-}
-
-int solve1(vector<vector<int>> &data) {
-
-    int res = 0;
-    for (auto &levels : data) {
-        res += checkLevels(levels);
-    }
-    return res;
-}
-
-
-int solve2(vector<vector<int>> &data) {
-    int res = 0;
-
-    for (auto &d : data) {
-        if (checkLevels(d)) {
-            res++;
-        } else {
-            for (size_t i = 0; i < d.size(); ++i) {
-                vector<int> copy(d);
-                copy.erase(copy.begin() + i);
-                if (checkLevels(copy)) {
-                    res++;
-                    break;
-                }
-            }
-        }
-    }
-    return res;
-}
-
 int main() {
-    auto fp = ifstream("data/day02.txt");
-    
-    vector<vector<int>> data;
+    auto fp = ifstream("data/day03.txt");
 
+    int res1 = 0;
+    int res2 = 0;
+    bool enabled = true;
     for (string line; getline(fp, line);) {
-        auto splitted = Split(line, ' ')
-            | transform([](auto ele) {
-                return std::stoi(ele);
-            })
-            | to<vector<int>>();
-        data.push_back(splitted);
+        istringstream stream(line);
     }
-    int res1 = solve1(data);
-    int res2 = solve2(data);
     print() << "Solution1: " << res1;
     print() << "Solution2: " << res2;
     return 0;
